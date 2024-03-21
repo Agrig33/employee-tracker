048fe114ee4af9a6c20fc168285a72ae962704fd
{airin grigorian} <agrigorian33@gmail.com>
2024-03-21 13:37:41 -0700

    added schema.sql with revisions

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,21 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERNCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERNCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERNCES employee(id) ON DELETE SET NULL
+);
