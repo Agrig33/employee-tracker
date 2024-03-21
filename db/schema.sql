2af09d1bb772967ac04ce6a85bb7e2479220ec4d
{airin grigorian} <agrigorian33@gmail.com>
2024-03-21 13:36:03 -0700

    added db folder

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KeyboardEvent,
+    name VARCHAR(30) NOT NULL
+);
+
