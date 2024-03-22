e9ca07f7beeb679387e0169f1dd0b7296719aad9
{airin grigorian} <agrigorian33@gmail.com>
2024-03-22 12:39:16 -0700

    revised lines 7, 27 and 29 on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 
-USE employees;
+USE employees
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KeyboardEvent,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
@@ -13,16 +13,18 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) 
+    FOREIGN KEY (department_id), 
     REFERNCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERNCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERNCES employee(id) ON DELETE SET NULL
+    FOREIGN KEY (role_id)
+    REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id), 
+    REFERENCES employee(id) ON DELETE SET NULL
 );
