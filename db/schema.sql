b129b994f7d493e7ed1e2c39eaedb264864764d9
{airin grigorian} <agrigorian33@gmail.com>
2024-03-24 00:20:36 -0700

    revised line 19 to employee on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
