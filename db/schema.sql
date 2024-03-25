232953ac1fb6f7dfb31795ea9fb164a2e0fb0fca
{airin grigorian} <agrigorian33@gmail.com>
2024-03-24 23:17:11 -0700

    tryng to fix code on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -25,4 +25,5 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id)
     REFERENCES role(id) ON DELETE CASCADE,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    
 );
